9676ab85804a6b67df1b782bb0074872f2f49584
Ruy Garcia <ruyg@simnang.com>
2021-03-25 08:41:22 -0700

    Tracking dist directory for packaging

---
+++ dist/types/src/utils/transform.utils.d.ts
@@ -0,0 +1,4 @@
+export declare const dateToDateTimeString: (date: Date) => string;
+export declare const dateToDateString: (date: Date) => string;
+export declare const dateToTimeString: (date: Date | string) => string;
+export declare const simpleArrayToString: (value: any[] | any) => string[] | any;
